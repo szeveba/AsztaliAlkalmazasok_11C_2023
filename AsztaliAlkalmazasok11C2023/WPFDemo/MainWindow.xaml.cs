aa2d3f2d9aeb28d543b3503e0e6876933268be86
Szegedi Barnabás <szeveba@gmail.com>
2024-02-02 12:25:38 +0100

    WPFDemo first milestone

--- AsztaliAlkalmazasok11C2023/WPFDemo/MainWindow.xaml.cs
+++ AsztaliAlkalmazasok11C2023/WPFDemo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFDemo.Views;
 
 namespace WPFDemo
 {
@@ -19,13 +21,92 @@ namespace WPFDemo
         public MainWindow()
         {
             InitializeComponent();
+            Thread.Sleep(1000);
         }
+        const string bookDBName = "books.txt";
 
         private void CreateBook_Click(object sender, RoutedEventArgs e)
         {
-            BookEditorWindow window = new BookEditorWindow();
-            window.ShowDialog();
-            //MessageBox.Show("Kattintottál", "Kattintás", MessageBoxButton.OK, MessageBoxImage.Warning);
+            var window = new BookEditorWindow();
+            if (window.ShowDialog() == true)
+            {
+                LB_Books.Items.Add(window.TB_Title.Text);
+                SaveBooks();
+            }
+        }
+
+        private void BTN_DeleteBook_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Biztos törölni szeretné a kiválasztott könyvet?", "Törlés", MessageBoxButton.YesNoCancel, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.Yes)
+            {
+                LB_Books.Items.Remove(LB_Books.SelectedItem);
+                SaveBooks();
+            }
+        }
+
+        private void SaveBooks()
+        {
+            var op = new List<string>();
+            foreach (string item in LB_Books.Items)
+            {
+                op.Add(item);
+            }
+            File.WriteAllLines(bookDBName, op);
+        }
+
+        private void BTN_EditBook_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new BookEditorWindow();
+            window.TB_Title.Text = (string)LB_Books.SelectedValue;
+            window.TB_Title.CaretIndex = window.TB_Title.Text.Length;
+            if (window.ShowDialog() == true)
+            {
+                var index = LB_Books.SelectedIndex;
+                LB_Books.Items.RemoveAt(index);
+                LB_Books.Items.Insert(index, window.TB_Title.Text);
+                SaveBooks();
+            }
+        }
+
+        private void LB_Books_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            BTN_EditBook_Click(sender, e);
+        }
+
+        private void LB_Books_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            BTN_DeleteBook.IsEnabled = LB_Books.SelectedItem != null;
+            BTN_EditBook.IsEnabled = LB_Books.SelectedItem != null;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(bookDBName))
+            {
+                var bookLines = File.ReadAllLines(bookDBName);
+                foreach (var line in bookLines)
+                {
+                    LB_Books.Items.Add(line);
+                }
+            }
+            else
+            {
+                File.Create(bookDBName);
+            }
+        }
+
+        private void LB_Books_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    BTN_EditBook_Click(sender, e);
+                    break;
+                case Key.Delete:
+                    BTN_DeleteBook_Click(sender, e);
+                    break;
+            }
         }
     }
 }
---
+++ AsztaliAlkalmazasok11C2023/WPFDemo/Views/BookEditorWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for BookEditorWindow.xaml
+    /// </summary>
+    public partial class BookEditorWindow : Window
+    {
+        public BookEditorWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void TB_Title_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BTN_Save.IsEnabled = !string.IsNullOrWhiteSpace(TB_Title.Text);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            TB_Title.Focus();
+        }
+
+        private void TB_Title_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter && !string.IsNullOrWhiteSpace(TB_Title.Text))
+            {
+                Save_Click(sender, e);
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
